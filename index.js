03b6113148cceb55298212b4aee78a830e28fbfd
Sameer Kanakia <skanakia1@gmail.com>
2018-03-26 21:35:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,5 +60,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (cardnumber === null) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    
+  }
 }
