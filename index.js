2dd8e2e1f8c538ded5af9dffc754b361c03891ae
Sameer Kanakia <skanakia1@gmail.com>
2018-03-26 21:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === null) {
+  if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     cart = []
