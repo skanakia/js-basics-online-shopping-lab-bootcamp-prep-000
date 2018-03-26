5e6069a4b4fc91661757ca5c87c10b58ee489104
Sameer Kanakia <skanakia1@gmail.com>
2018-03-26 20:12:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  var itemName = item
  var itemPrice = Math.floor(Math.random() * Math.floor(100))
- cart.push({'itemName: ${itemName': 'itemPrice: ${itemPrice}'})
+ cart.push({'itemName: ${itemName}': 'itemPrice: ${itemPrice}'})
  return "${itemName} has been added to your cart."
 }
 
