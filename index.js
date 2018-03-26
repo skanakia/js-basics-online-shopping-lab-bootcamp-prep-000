3d27778496c884ffa86bb44930c8d7e697c2e45e
Sameer Kanakia <skanakia1@gmail.com>
2018-03-26 21:44:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    cart = []
     var order = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     return order
+    cart = []
   }
 }
